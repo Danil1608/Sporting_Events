598295500f830138a8831ea27b20f2600217d119
Danil1608 <dan-usoev2015@mail.ru>
2021-12-11 15:02:52 +0300

    шаблоны представлений и контроллер для соревнований

---
+++ Controllers/CompetitionController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Sporting_Events.Models;
+
+namespace Sporting_Events.Controllers
+{
+    public class CompetitionController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CompetitionController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Competition
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Competitions.Include(c => c.Accounts).Include(c => c.CompetitionType).ToListAsync());
+        }
+
+        // GET: Competition/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var competition = await _context.Competitions
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (competition == null)
+            {
+                return NotFound();
+            }
+
+            return View(competition);
+        }
+
+        // GET: Competition/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Competition/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,Date,ExpDate,Location,MembersCount,PrizePool,TypeId")] Competition competition)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(competition);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(competition);
+        }
+
+        // GET: Competition/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var competition = await _context.Competitions.FindAsync(id);
+            if (competition == null)
+            {
+                return NotFound();
+            }
+            return View(competition);
+        }
+
+        // POST: Competition/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Date,ExpDate,Location,MembersCount,PrizePool,TypeId")] Competition competition)
+        {
+            if (id != competition.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(competition);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CompetitionExists(competition.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(competition);
+        }
+
+        // GET: Competition/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var competition = await _context.Competitions
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (competition == null)
+            {
+                return NotFound();
+            }
+
+            return View(competition);
+        }
+
+        // POST: Competition/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var competition = await _context.Competitions.FindAsync(id);
+            _context.Competitions.Remove(competition);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CompetitionExists(int id)
+        {
+            return _context.Competitions.Any(e => e.Id == id);
+        }
+    }
+}
